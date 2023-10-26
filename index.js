7b885a2664d94bdb10510ccd9176395b43188d1b
Partik Malasi <119388816+PartikMalasi@users.noreply.github.com>
2023-10-26 09:06:32 +0530

    added public

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Express from "express";
 import bodyParser from "body-parser";
 const app=Express();
 const port=3000;
-
+const hostname='0.0.0.0';
 const currentDate = new Date();
 const months = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
 const dates = currentDate.getDate()+" "+months[(currentDate.getMonth() + 1)]+", " 
