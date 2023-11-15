e5b0c37050a63ed4b0b799cad8f2f6c2c1a6d86d
Partik Malasi <119388816+PartikMalasi@users.noreply.github.com>
2023-11-15 12:32:18 +0530

    done

--- index.js
+++ index.js
@@ -1,50 +1,62 @@
 import Express from "express";
 import bodyParser from "body-parser";
-const app=Express();
-const port=3000;
-const hostname='0.0.0.0';
+const app = Express();
+const port = 3000;
+const hostname = "0.0.0.0";
+
 const currentDate = new Date();
-const months = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
-const dates = currentDate.getDate()+" "+months[(currentDate.getMonth() + 1)]+", " 
-+currentDate.getFullYear();
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const dates =
+  currentDate.getDate() +
+  " " +
+  months[currentDate.getMonth() + 1] +
+  ", " +
+  currentDate.getFullYear();
+var c = [];
 
 app.use(Express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var c=[];
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
+  res.render("index.ejs", { htmlContant: c, date: dates });
+});
 
-   
-    
-    res.render("index.ejs",{htmlContant: c,date: dates });
-})
-var a=1;
-app.post("/check",(req,res)=>{
-    
-    c.push(req.body["task"]);
-   
-       
-       const data= { htmlContant : c,date: dates,count: a };
-       
+var p = 1;
+app.post("/check", (req, res) => {
+  if (req.body.task != []) {
+    c.push({ task: req.body.task, id: p });
+    p++;
+  }
+  //   console.log(c);
+  const data = { htmlContant: c, date: dates };
+  res.redirect("/");
+  // res.render("index.ejs",data);
+});
 
-    res.render("index.ejs",data);
-})
+app.get("/delete", (req, res) => {
+  const taskId = parseInt(req.query.taskId);
+  console.log(taskId);
+  c = c.filter((task) => task.id !== taskId);
+  console.log("delete " + c);
+  //    c=newArrayWithoutTask;
+  const data = { htmlContant: c, date: dates };
+  res.redirect("/");
+  // res.render("index.ejs",data);
+});
 
-app.get("/delete",(req,res)=>{
-    const currentDate = new Date();
-    const months = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
-    const dates = currentDate.getDate()+" "+months[(currentDate.getMonth() + 1)]+", " 
-+currentDate.getFullYear();
-    
-   
-        c.splice(a-1, 1);
-        console.log(c);
-       const data= { htmlContant : c,date: dates, count: a};
-       
-
-    res.render("index.ejs",data);
-})
-app.listen(port,()=>{
-    console.log("done");
-})
-
+app.listen(port, () => {
+  console.log("done");
+});
